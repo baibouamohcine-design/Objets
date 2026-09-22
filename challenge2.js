9a63bd4aa99eca7ebb61458c2a528b85029e0e1d
unknown <baibouamohamed@gmail.com>
2026-09-22 12:20:35 +0200

    Objet avec Tableau

---
+++ challenge2.js
@@ -0,0 +1,12 @@
+
+const etudiantDetails = {
+    nom : "Ahmadi",
+    prenom : "Anas",
+    filiere: "Devellepemnt",
+    notes : [10,18, 18,20],
+}
+
+console.log(etudiantDetails.nom)
+console.log(etudiantDetails.prenom)
+console.log(etudiantDetails.filiere)
+console.log(etudiantDetails.notes)
