f2fd137ee3aa73e153b325b39ad3eae6dcc22f01
unknown <baibouamohamed@gmail.com>
2026-09-22 13:23:30 +0200

    Modification des Propriétés d'un Objet

---
+++ challenge5.js
@@ -0,0 +1,11 @@
+const etudiantNotes = {
+    geography : 16,
+    historic : 12,
+    math : 8,
+    francais:  17,
+    arabe : 6,
+    philosophie : 2 ,
+}
+etudiantNotes.philosophie = 8
+etudiantNotes.math = 16
+console.log(etudiantNotes)
